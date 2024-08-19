67445e5ba4480baf438751e3c2801b44eb82137e
tomcat <tommypo26@gmail.com>
2024-08-19 13:31:37 +0300

    Add modal for desktop

--- src/app/layout/baseLayout.tsx
+++ src/app/layout/baseLayout.tsx
@@ -1,7 +1,6 @@
 import styles from './layout.module.scss';
 import cnBind from 'classnames/bind';
 import { Outlet } from 'react-router-dom';
-import { Toast } from '../../components/toast/toast.tsx';
 
 const cx = cnBind.bind(styles);
 
@@ -9,7 +8,6 @@ export function BaseLayout() {
   return (
     <div className={cx('app-container')}>
       <Outlet />
-      <Toast />
     </div>
   );
 }
