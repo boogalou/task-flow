5f57ed2854c48ff14102fc6caaaba46f2de27a18
tomcat <tommypo26@gmail.com>
2024-08-31 09:13:22 +0300

    Add date formatting to subtitle

--- src/components/main-content/main-content.tsx
+++ src/components/main-content/main-content.tsx
@@ -1,5 +1,6 @@
 import styles from './main-content.module.scss';
 import cnBind from 'classnames/bind';
+import { addDays, format } from 'date-fns';
 import { Button } from '../../shared/ui-kit/button/Button.tsx';
 import { Icon } from '../../shared/ui-kit/icon/icon.tsx';
 import { TaskForm } from '../task-form/taskForm.tsx';
@@ -9,22 +10,34 @@ import { useModal } from '../../shared/ui-kit/modal/useModal.ts';
 import { TasksList } from '../task/tasks-list.tsx';
 import { useAppSelector } from '../../app/redux/reduxHooks.ts';
 import { selectFilter } from '../task/model/taskSlice.ts';
+import { useEffect, useState } from 'react';
 
 const cx = cnBind.bind(styles);
 
 export function MainContent() {
   const { isOpen, openModal, closeModal } = useModal();
+  const [subtitleDate, setSubtitleDate] = useState('');
   const filters = useAppSelector(selectFilter);
 
   const handleOpenModal = () => {
     openModal();
   };
 
+  useEffect(() => {
+    if (filters.date === 'today') {
+      setSubtitleDate(format(new Date(), 'MMM dd'));
+    } else if (filters.date === 'week') {
+      const startDate = new Date();
+      const endDate = addDays(startDate, 6);
+      setSubtitleDate(`${format(startDate, 'MMM dd')} - ${format(endDate, 'MMM dd')}`);
+    }
+  }, [filters]);
+
   return (
     <div className={cx('content')}>
       <header className={cx('content__header')}>
         <h2 className={cx('content__title')}>{filters.date}</h2>
-        <div className={cx('content__subtitle')}>Aug 13, Today 7</div>
+        <div className={cx('content__subtitle')}>{subtitleDate}</div>
       </header>
       <TasksList />
       <Button className={cx('content__button', 'content__button-add')} onClick={handleOpenModal}>
