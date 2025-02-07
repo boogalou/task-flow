ef7a92fe206dfae6a43cf4459140010cbf3f62b2
tomcat <tommypo26@gmail.com>
2025-02-07 23:12:11 +0300

    misc

--- src/entities/auth/model/registration.thunk.ts
+++ src/entities/auth/model/registration.thunk.ts
@@ -1,7 +1,22 @@
 import { createAsyncAction } from 'shared/lib/thunk.adapter.ts';
 import { authService } from 'shared/api/auth.service.ts';
+import { CaseReducer, PayloadAction } from '@reduxjs/toolkit';
+import { AuthState } from 'entities/auth/model/auth.slice.ts';
+import { AuthResponse } from 'shared/types/types.ts';
 
 export const registrationRequest = createAsyncAction({
   actionType: 'auth/registration',
   method: authService.registration,
 });
+
+export const handleRegistrationPending: CaseReducer<AuthState> = (state) => {
+  state.authFetchStatus = 'loading';
+  state.error = null;
+};
+
+export const handleRegistrationFulfilled: CaseReducer<AuthState, PayloadAction<AuthResponse>> = (
+  state,
+) => {
+  state.authFetchStatus = 'loading';
+  state.error = null;
+};
